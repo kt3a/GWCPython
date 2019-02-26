16e4cdab5553f0f840443108e9868e46e816cee5
KatieTooher <katie.tooher@marquette.edu>
2019-02-26 09:21:43 -0600

    changes

--- ClassDemo.py
+++ ClassDemo.py
@@ -1,10 +1,14 @@
-from tkinter import*
+import tkinter
 from tkinter import Button
 
 #define the quit function
 def set_quit():
    top.destroy()
     
+#to show text
+def show_text():
+   message.insert(0,e.get())
+
 
 #Layout
 top = tkinter.Tk()
@@ -14,6 +18,24 @@ top.geometry("300x300")
 label = tkinter.Label(top, text = "Hello World!")
 top.title("First GUI")
 label.pack()
+
+
+
+#Entry Widget
+e = tkinter.Entry(top)
+e.pack()
+
+#string_input = e.get()
+text = tkinter.Label(top, text = "Answer:").pack()
+
+#make a box to display the message
+message = tkinter.Entry(top)
+message.pack()
+
+#make a button that will display the message when pushed
+ans = tkinter.Button(top, text = "Show Message", command = show_text)
+ans.pack()
+
 
 #button
 qbutton = tkinter.Button(top, text = "Quit", command = set_quit)
