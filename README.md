b37cb08ec7db4f270af3fb9c8aa1de58c9cc95ee
KatieTooher <katie.tooher@marquette.edu>
2019-03-19 16:26:38 -0500

    changes

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # GWCPython
+
+Hello this github repository is for the use of the students of Level 1 Girls Who Code club at Marquette Univeristy. 
--- drawing_game.py
+++ drawing_game.py
@@ -1,21 +1,24 @@
 from turtle import *
 from tkinter import *
 
-
+##This creates the window 
 window = Tk()
-window.title("Draw!")
+window.title("Drawing Game")                 ##change the title if you'd like
+
+##the canvas widget allows us to place a turtle drawing onto our tkinter gui
 cv = Canvas(window,width=600,height=600)
 cv.pack(side = LEFT)
 
-
-
+#RawTurtle instead of regular Turtle() (as we used in our demo) allows for adding a
+##section called a turtle screen  -- this we place onto the canvas widget so we can draw 
 t = RawTurtle(cv)
-t.color("blue")
+t.color("pink")             ##try adding and changing colors
 screen = t.getscreen()
 
-
+##this sets the coordinates of where the turtle drawing pen will start on the screen
+##try changing the coordinates to move where you want the drawing to start
 screen.setworldcoordinates(0,0,600,600)
-screen.bgcolor("white")
+screen.bgcolor("white")             ##try changing the color of your drawing screen
 
 
 # A frame is an invisible widget that holds other widgets. 
@@ -23,14 +26,9 @@ screen.bgcolor("white")
 frame = Frame(window)
 frame.pack(side = RIGHT,fill=BOTH)
 
-
-pointLabel = Label(frame,text="Width")
+##A text label to show what the text field is for
+pointLabel = Label(frame,text="Choose Thickness of the Pen!")
 pointLabel.pack()
-
-#------------------------------------------------------------------------------------------------------
-##add color change buttons here or more lables 
-
-
 
 
 
@@ -44,31 +42,42 @@ pointEntry = Entry(frame,textvariable=pointSize)
 pointEntry.pack()
 pointSize.set(str(1))
 
+#------------------------------------------------------------------------------------------------------
+##try to make this code your own by adding buttons that could change the color of the pen
+##here and below
+##Try to make more lables or other functionality!! Have fun with this!
 
-# This is an event handler. Handling the quit button press results in destroying the window
-# and quitting the application. 
+
+
+
+
+
+
+
+
+
+##This function allows you to quit the gui 
 def quitHandler():
     window.destroy()
     window.quit() 
-      
-   # This is how a button is created in the frame. The quitHandler is the event handler for button
-   # presses of the "Quit" button.
+
+##this is the button you press to use the quitHandler function defined above
 quitButton = Button(frame, text = "Quit", command=quitHandler)
 quitButton.pack()
    
-   # Here is another event handler. This one handles mouse clicks on the screen.
+# Here is another event handler. This one handles mouse clicks on the screen.
 def clickHandler(x,y): 
     # When a mouse click occurs, get the pointSize entry value and set the width of the 
     # turtle called "t" to the pointSize value. The int(pointSize.get()) is needed because
     # the width is an integer, but the entry widget stores it as a string.
     t.width(int(pointSize.get()))
     t.goto(x,y)
+
       
-# Here is how we tie the clickHandler to mouse clicks.
+#This calls the clickHandler function and connects the function to mouse clicks
 screen.onclick(clickHandler)
    
-# Finally, this code is last. It tells the application to enter its event processing loop
-# so the application will respond to events. 
+#This statement ends the gui code 
 tkinter.mainloop()
 
 
